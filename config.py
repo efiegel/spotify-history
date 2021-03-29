baf731848f4125e94181028e30e2a6e8917b5907
ericfiegel <efiegel01@gmail.com>
2021-03-28 18:10:42 -0700

    changed db url env var

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'change-me'
-    SQLALCHEMY_DATABASE_URI = os.environ['DB_URL']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
