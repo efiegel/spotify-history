e87aa63d39337a4539662704e21ff505bf5c1925
ericfiegel <efiegel01@gmail.com>
2021-03-30 14:26:20 -0700

    removed test dag from app home

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-from dags.spotify_etl import run_spotify_etl
-
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
@@ -14,7 +12,6 @@ from models import *
 
 @app.route("/")
 def home():
-    run_spotify_etl()
     return "spotify history app"
 
 if __name__ == '__main__':
